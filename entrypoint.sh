5fddef31be24648be8658b68dfbd1f60037e00b7
Tere <teresa@okteto.com>
2024-05-28 15:15:57 +0200

    Add log-level to input (#10)
    * add log-level input
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    * update error message for unsupported log level
    
    ---------
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,8 +21,25 @@ if [ ! -z $namespace ]; then
   args="--namespace=$namespace"
 fi
 
-echo running: okteto context use --token=$token $args $url
-okteto context use --token=$token $args $url 
+log_level=$4
+if [ ! -z "$log_level" ]; then
+  if [ "$log_level" = "debug" ] || [ "$log_level" = "info" ] || [ "$log_level" = "warn" ] || [ "$log_level" = "error" ] ; then
+    log_level="--log-level ${log_level}"
+  else
+    echo "unsupported log-level ${log_level}, supported options are: debug, info, warn, error"
+    exit 1
+  fi
+fi
+
+# https://docs.github.com/en/actions/monitoring-and-troubleshooting-workflows/enabling-debug-logging
+# https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
+if [ "${RUNNER_DEBUG}" = "1" ]; then
+  log_level="--log-level debug"
+fi
+
+
+echo running: okteto context use --token=$token $args $url $log_level
+okteto context use --token=$token $args $url $log_level
 
 echo running: okteto kubeconfig
 eval okteto kubeconfig
